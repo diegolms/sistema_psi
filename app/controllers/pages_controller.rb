27b32db7d495c88aefbf6d3f95c88d8c7ac41b44
Diego Lopes <diego.silva@control.eng.br>
2019-05-20 17:33:43 -0300

    lancamentos a pagar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ApplicationController
  
   def index
     @lancamentos_receita = Lancamento.where("ativo = #{Constantes::ATIVO} and tipo = #{Lancamento::TIPO_LANCAMENTO_RECEITA}").sum(:valor) 
-    @lancamentos_despesa = Lancamento.where("ativo = #{Constantes::ATIVO} and tipo = #{Lancamento::TIPO_LANCAMENTO_DESPESA}").sum(:valor)
+    @lancamentos_despesa = Lancamento.where("ativo = #{Constantes::ATIVO} and tipo = #{Lancamento::TIPO_LANCAMENTO_DESPESA} and data_pagamento is not null").sum(:valor)
+	@lancamentos_despesa = Lancamento.where("ativo = #{Constantes::ATIVO} and tipo = #{Lancamento::TIPO_LANCAMENTO_DESPESA} and data_pagamento is null").sum(:valor)
 	@caixa = Caixa.last.valor
     @pessoas = Pessoa.where("ativo = #{Constantes::ATIVO} and id not in (1)").size	
   end
