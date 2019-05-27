74d75e7d57a94da6b850485ef6652da6efb763c5
Diego Lopes <diego.silva@control.eng.br>
2019-05-27 15:21:29 -0300

    anexos com carrierwave

--- app/controllers/lancamentos_controller.rb
+++ app/controllers/lancamentos_controller.rb
@@ -1,12 +1,6 @@
 class LancamentosController < ApplicationController
   before_action :set_lancamento, only: [:show, :edit, :update, :destroy]
   
-  before_action :check_configuration
-
-  def check_configuration
-    render 'configuration_missing' if Cloudinary.config.api_key.blank?
-  end
-
   # GET /lancamentos
   # GET /lancamentos.json
   def index
@@ -35,21 +29,6 @@ class LancamentosController < ApplicationController
   def new
     @lancamento = Lancamento.new
 	@lancamento.tipo = Lancamento::TIPO_LANCAMENTO_RECEITA
-
-    if params[:unsigned].present?
-      @unsigned = true
-      # make sure we have the appropriate preset
-      @preset_name = "sample_" + Digest::SHA1.hexdigest(Cloudinary.config.api_key + Cloudinary.config.api_secret)
-      begin
-        preset = Cloudinary::Api.upload_preset(@preset_name)
-        if !preset["settings"]["return_delete_token"]
-          Cloudinary::Api.update_upload_preset(@preset_name, :return_delete_token=>true)
-        end
-      rescue
-        # An upload preset may contain (almost) all parameters that are used in upload. The following is just for illustration purposes
-        Cloudinary::Api.create_upload_preset(:name => @preset_name, :unsigned => true, :folder => "preset_folder", :return_delete_token=>true)
-      end
-    end
   end
 
   # GET /lancamentos/1/edit
